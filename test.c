89627aa198085f0d79b8635a2825e4b9f3adbe07
diegodiaz1256 <fernandezdiego3758@gmail.com>
2021-10-22 16:15:31 +0200

    funcionan nos comandos basicos

--- test.c
+++ test.c
@@ -1,39 +1,62 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#define _GNU_SOURCE
 #include "parser.h"
 
-int
-main(void) {
+int main(void)
+{
 	char buf[1024];
-	tline * line;
-	int i,j;
+	tline *line;
+	int i, j;
+	pid_t pid;
 
-	printf("==> ");	
-	while (fgets(buf, 1024, stdin)) {
-		
+	printf("msh> ");
+	while (fgets(buf, 1024, stdin))
+	{
+
 		line = tokenize(buf);
-		if (line==NULL) {
+		if (line == NULL)
+		{
 			continue;
 		}
-		if (line->redirect_input != NULL) {
+		if (line->redirect_input != NULL)
+		{
 			printf("redirección de entrada: %s\n", line->redirect_input);
 		}
-		if (line->redirect_output != NULL) {
+		if (line->redirect_output != NULL)
+		{
 			printf("redirección de salida: %s\n", line->redirect_output);
 		}
-		if (line->redirect_error != NULL) {
+		if (line->redirect_error != NULL)
+		{
 			printf("redirección de error: %s\n", line->redirect_error);
 		}
-		if (line->background) {
+		if (line->background)
+		{
 			printf("comando a ejecutarse en background\n");
-		} 
-		for (i=0; i<line->ncommands; i++) {
+		}
+		for (i = 0; i < line->ncommands; i++)
+		{
 			printf("orden %d (%s):\n", i, line->commands[i].filename);
-			for (j=0; j<line->commands[i].argc; j++) {
+			for (j = 0; j < line->commands[i].argc; j++)
+			{
 				printf("  argumento %d: %s\n", j, line->commands[i].argv[j]);
 			}
 		}
-		printf("==> ");	
+		if (line->ncommands > 0)
+		{
+			pid = fork();
+			if(pid==0){
+				execvp(line->commands[0].filename, line->commands->argv);
+				exit(0);
+			}else{
+				waitpid(pid,NULL,0);
+			}
+		}
+		
+		printf("msh> ");
 	}
 	return 0;
 }
